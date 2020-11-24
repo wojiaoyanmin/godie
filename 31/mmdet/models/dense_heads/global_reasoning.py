3284121478e17724f67ad34d8116c352eca17abd
wojiaoyanmin <358787362@qq.com>
2020-11-24 11:24:57 +0800

    0

--- 31/mmdet/models/dense_heads/global_reasoning.py
+++ 31/mmdet/models/dense_heads/global_reasoning.py
@@ -140,14 +140,14 @@ class GloRe_Unit(nn.Module):
         # (n, num_state, h, w) -> (n, num_in, h, w)
         x_state = self.recover(x_state)
         x_state = self.conv_extend(x_state)
-        for i in range(x_state.shape[1]):
-            plt.subplot(1,3,1)
-            plt.imshow(self.blocker(x_state)[0][i].cpu().numpy())
-            plt.subplot(1,3,2)
-            plt.imshow(feats_all[0][i].cpu().numpy())
-            plt.subplot(1,3,3)
-            plt.imshow((self.blocker(x_state)+feats_all)[0][i].cpu().numpy())
-            plt.show()
+        # for i in range(x_state.shape[1]):
+        #     plt.subplot(1,3,1)
+        #     plt.imshow(self.blocker(x_state)[0][i].cpu().numpy())
+        #     plt.subplot(1,3,2)
+        #     plt.imshow(feats_all[0][i].cpu().numpy())
+        #     plt.subplot(1,3,3)
+        #     plt.imshow((self.blocker(x_state)+feats_all)[0][i].cpu().numpy())
+        #     plt.show()
         out = self.blocker(x_state)#+feats_all
 
         return out
